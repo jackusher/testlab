2269384194d897fc80a0c983b83d62d50f83697a
Jack Usher <Jack@Jacks-PowerBook.local>
2016-09-25 18:28:02 +0100

    Removed a few syntax errors in the Masonry initialisation jQuery.

--- js/masonry-properties.js
+++ js/masonry-properties.js
@@ -1,15 +1,15 @@
-jQuery(window).load(function() {
+jQuery(window).load(function () {
 
 var container = document.querySelector('#section1-wrap');
-var msnry = new Masonry( container, {
+var msnry = new Masonry(container, {
 	itemSelector: '.front-article',
-	columnWidth: 1,
+	columnWidth: 1
 });
 
 var container = document.querySelector('#section2-wrap');
-var msnry = new Masonry( container, {
+var msnry = new Masonry(container, {
 	itemSelector: '.front-article',
-	columnWidth: 1,
+	columnWidth: 1
 });
 
 });
