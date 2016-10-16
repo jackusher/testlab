4d35b28f9758077e611149d5c2b1c63deb71dca4
Jack Usher <Jack@Jacks-PowerBook.local>
2016-10-16 01:41:16 +0100

    Used masonry to fix article alignments in homepage columns.

--- js/masonry-properties.js
+++ js/masonry-properties.js
@@ -1,8 +1,8 @@
 jQuery(window).load(function () {
 
-var container = document.querySelector('#masonry-container');
+var container = document.querySelector('#section2-content');
 var msnry = new Masonry(container, {
-	itemSelector: '.masonry-item',
+	itemSelector: '.front-article',
 	columnWidth: 1
 });
 
