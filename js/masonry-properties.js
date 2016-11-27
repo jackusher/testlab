74ca0d16adbcc290e5988855ffede1a545ade646
Jack Usher <Jack@Jacks-PowerBook.default>
2016-11-27 23:36:23 +0000

    Limited border height, added front category header colours.

--- js/masonry-properties.js
+++ js/masonry-properties.js
@@ -1,6 +1,18 @@
 jQuery(window).load(function () {
 
+var container = document.querySelector('#section1-content');
+var msnry = new Masonry(container, {
+	itemSelector: '.front-article',
+	columnWidth: 1
+});
+
 var container = document.querySelector('#section2-content');
+var msnry = new Masonry(container, {
+	itemSelector: '.front-article',
+	columnWidth: 1
+});
+
+var container = document.querySelector('#section3-content');
 var msnry = new Masonry(container, {
 	itemSelector: '.front-article',
 	columnWidth: 1
