f37dd55453c8bc5529838f8fdf80927137157ddc
Jack Usher <Jack@Jacks-PowerBook.default>
2017-03-06 15:25:13 +0000

    Created stop point for fixed section headers on homepage.

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@
 $(window).scroll(function() {    
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 135) {
+    if (scroll >= 135 && scroll <= 1645 ) {
         $(".front-head").addClass("front-fixed");
     } else {
         $(".front-head").removeClass("front-fixed");
