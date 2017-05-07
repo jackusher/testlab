e54c03b1616d30c7d2f62a7243c462310618ffdd
Jack Usher <Jack@Jacks-PowerBook.default>
2017-05-07 11:20:13 +0100

    Added bubble text-domains.

--- comments.php
+++ comments.php
@@ -20,7 +20,7 @@ if ( post_password_required() )
     <?php if ( have_comments() ) : ?>
         <h2 class="comments-title">
             <?php
-                printf( _nx( 'One thought on %2$s', '%1$s thoughts on %2$s', get_comments_number(), 'comments title', 'twentythirteen' ),
+                printf( _nx( 'One thought on %2$s', '%1$s thoughts on %2$s', get_comments_number(), 'comments title', 'bubble3' ),
                     number_format_i18n( get_comments_number() ), '<span> this article. </span>' );
             ?>
         </h2>
@@ -39,14 +39,14 @@ if ( post_password_required() )
 		// Are there comments to navigate through?
 		if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
 			<nav class="navigation comment-navigation" role="navigation">
-				<h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'twentythirteen' ); ?></h1>
-				<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentythirteen' ) ); ?></div>
-				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentythirteen' ) ); ?></div>
+				<h1 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'bubble3' ); ?></h1>
+				<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'bubble3' ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'bubble3' ) ); ?></div>
 			</nav><!-- /comment-navigation -->
 		<?php endif; // Check for comment navigation ?>
  
         <?php if ( ! comments_open() && get_comments_number() ) : ?>
-        	<p class="no-comments"><?php _e( 'Comments are closed.' , 'twentythirteen' ); ?></p>
+        	<p class="no-comments"><?php _e( 'Comments are closed.' , 'bubble3' ); ?></p>
         <?php endif; ?>
  
     <?php endif; // have_comments() ?>
